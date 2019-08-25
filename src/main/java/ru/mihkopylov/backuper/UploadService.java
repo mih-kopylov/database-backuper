d44b440914167ab797031d5bb531172a4007caed
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-08-25 17:50:18 +0300

    Switch to Java 8

--- src/main/java/ru/mihkopylov/backuper/UploadService.java
+++ src/main/java/ru/mihkopylov/backuper/UploadService.java
@@ -8,8 +8,8 @@ import com.yandex.disk.rest.json.DiskInfo;
 import com.yandex.disk.rest.json.Link;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -45,7 +45,7 @@ public class UploadService {
     @SneakyThrows
     private void createMissedDirectories( @NonNull RestClient restClient, @NonNull String uploadPath ) {
         log.info( "creating directories for {}", uploadPath );
-        List<String> paths = List.of( uploadPath.split( "/" ) );
+        List<String> paths = new ArrayList<>( Arrays.asList(uploadPath.split( "/" ) ));
         for (int i = 1; i < paths.size(); i++) {
             String tempPath = String.join( "/", paths.subList( 0, i ) );
             try {
@@ -62,11 +62,11 @@ public class UploadService {
     private String getUploadPath( @NonNull File file ) {
         String fileYearMonth = file.getName().substring( 0, 7 );
         String fileYearMonthDay = file.getName().substring( 0, 10 );
-        List<String> paths = new ArrayList<>( List.of( configuration.getDiskPath().split( "/" ) ) );
-        paths.addAll( List.of( fileYearMonth, fileYearMonthDay, file.getName() ) );
+        List<String> paths = new ArrayList<>( Arrays.asList( configuration.getDiskPath().split( "/" ) ) );
+        paths.addAll( Arrays.asList( fileYearMonth, fileYearMonthDay, file.getName() ) );
         return paths.stream()
                 .map( String :: trim )
-                .filter( Predicate.not( String :: isBlank ) )
+                .filter( o->!o.equals( "" ) )
                 .collect( Collectors.joining( "/" ) );
     }
 
