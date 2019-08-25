57cf380472fc34993bfd865cb3ec9f01fc740ce0
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-08-25 08:13:41 +0300

    Put backup file to directories with current year month and date

--- src/main/java/ru/mihkopylov/backuper/UploadService.java
+++ src/main/java/ru/mihkopylov/backuper/UploadService.java
@@ -1,11 +1,16 @@
 package ru.mihkopylov.backuper;
 
 import com.yandex.disk.rest.Credentials;
-import com.yandex.disk.rest.ProgressListener;
+import com.yandex.disk.rest.ResourcesArgs;
 import com.yandex.disk.rest.RestClient;
+import com.yandex.disk.rest.exceptions.http.NotFoundException;
 import com.yandex.disk.rest.json.DiskInfo;
 import com.yandex.disk.rest.json.Link;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.SneakyThrows;
@@ -24,10 +29,11 @@ public class UploadService {
         log.info( "uploading backup file {}", file );
         RestClient restClient =
                 new RestClient( new Credentials( configuration.getDiskUser(), configuration.getDiskToken() ) );
-        log.info( "free space on yandex disk: {} Mb", getFreeSpace( restClient ) );
-        String uploadPath = configuration.getDiskPath() + "/" + file.getName();
+        log.info( "free space on yandex disk: {} Mb", getFreeSpaceMb( restClient ) );
+        String uploadPath = getUploadPath( file );
+        createMissedDirectories( restClient, uploadPath );
         Link uploadLink = restClient.getUploadLink( uploadPath, true );
-        restClient.uploadFile( uploadLink, false, file, new LoggingProgressListener() );
+        restClient.uploadFile( uploadLink, false, file, null );
         log.info( "backup uploaded" );
         if (file.delete()) {
             log.info( "backup file {} deleted", file );
@@ -37,20 +43,36 @@ public class UploadService {
     }
 
     @SneakyThrows
-    private long getFreeSpace( @NonNull RestClient restClient ) {
-        DiskInfo diskInfo = restClient.getDiskInfo();
-        return (diskInfo.getTotalSpace() - diskInfo.getUsedSpace()) / 1024 / 1024;
+    private void createMissedDirectories( @NonNull RestClient restClient, @NonNull String uploadPath ) {
+        log.info( "creating directories for {}", uploadPath );
+        List<String> paths = List.of( uploadPath.split( "/" ) );
+        for (int i = 1; i < paths.size(); i++) {
+            String tempPath = String.join( "/", paths.subList( 0, i ) );
+            try {
+                restClient.getResources( new ResourcesArgs.Builder().setPath( tempPath ).setLimit( 10000 ).build() );
+                log.info( "path {} exists", tempPath );
+            } catch (NotFoundException e) {
+                restClient.makeFolder( tempPath );
+                log.info( "path {} created", tempPath );
+            }
+        }
     }
 
-    private static class LoggingProgressListener implements ProgressListener {
-        @Override
-        public void updateProgress( long loaded, long total ) {
-            log.info( "uploaded {}%: {} of {}", loaded * 100 / total, loaded, total );
-        }
+    @NonNull
+    private String getUploadPath( @NonNull File file ) {
+        String fileYearMonth = file.getName().substring( 0, 7 );
+        String fileYearMonthDay = file.getName().substring( 0, 10 );
+        List<String> paths = new ArrayList<>( List.of( configuration.getDiskPath().split( "/" ) ) );
+        paths.addAll( List.of( fileYearMonth, fileYearMonthDay, file.getName() ) );
+        return paths.stream()
+                .map( String :: trim )
+                .filter( Predicate.not( String :: isBlank ) )
+                .collect( Collectors.joining( "/" ) );
+    }
 
-        @Override
-        public boolean hasCancelled() {
-            return false;
-        }
+    @SneakyThrows
+    private long getFreeSpaceMb( @NonNull RestClient restClient ) {
+        DiskInfo diskInfo = restClient.getDiskInfo();
+        return (diskInfo.getTotalSpace() - diskInfo.getUsedSpace()) / 1024 / 1024;
     }
 }
